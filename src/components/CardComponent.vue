2ffade5f497434b397f88f7f6dfc75bd50e042d1
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-09-03 18:18:17 -0400

    feat: add bg-dark e text-white no card

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -16,7 +16,7 @@ const imgUrl = `https://image.tmdb.org/t/p/w500${card.card.poster_path}`;
 <template>
     <div class="card d-flex flex-column h-100">
         <img :src="imgUrl" class="card-img-top" :alt="card.card.title">
-        <div class="card-body d-flex flex-column justify-content-between">
+        <div class="card-body bg-dark text-white d-flex flex-column justify-content-between">
             <h5 class="card-title">
                 {{ card.card.title }}
             </h5>
